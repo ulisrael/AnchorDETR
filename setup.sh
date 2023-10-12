0676eb11a4ae4f41e2d1661c84319e2bbad1b8cc
damaggu <markusm2342@gmail.com>
2023-10-11 17:12:08 -0700

    adding sam stuff to setup

--- setup.sh
+++ setup.sh
@@ -3,9 +3,19 @@
 #python voc2coco.py --split val
 #python voc2coco.py --split test
 
-mkdir data/coco/annotations
-mkdir data/coco/train
-mkdir data/coco/val
+#mkdir data/coco/annotations
+#mkdir data/coco/train
+#mkdir data/coco/val
 
-cp -r data/VOCdevkit/VOC2007/JPEGImages/* data/coco/train/
-cp -r data/VOCdevkit/VOC2007/JPEGImages/* data/coco/val/
+#cp -r data/VOCdevkit/VOC2007/JPEGImages/* data/coco/train/
+#cp -r data/VOCdevkit/VOC2007/JPEGImages/* data/coco/val/
+
+# sam stuff
+git clone https://github.com/damaggu/segment-anything.git
+mkdir pretrained_models
+wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth -P pretrained_models/
+
+mv segment-anything segment_anything
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+export PYTHONPATH=$PYTHONPATH:$(pwd)/segment_anything
+export PYTHONPATH=$PYTHONPATH:$(pwd)/segment_anything/segment_anything
