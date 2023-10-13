4bbda892ba6fc324005b91e481b00376d20bcbc0
damaggu <markusm2342@gmail.com>
2023-10-13 10:48:37 -0700

    adj run for cellpose

--- anchorSAM_cellruns.sh
+++ anchorSAM_cellruns.sh
@@ -1,6 +1,12 @@
-python main.py --device_num 2 --coco_path ./data/coco/ --output_dir rerun_anchor_VOC_resnet_UriahParams_regularLRdrop \
---wandb_run_name rerun_anchor_VOC_resnet_UriahParams_regularLRdrop \
---backbone SAM --num_query_position 50 --batch_size 4 --num_workers 4 --epochs 50000 --eval_checkpoint_period 5 \
+python main.py --backbone SAM \
+--device_num 2 \
+--num_query_position 50 \
+--batch_size 4 \
+--num_workers 4 \
+--epochs 50000 \
+--eval_checkpoint_period 5 \
+--output_dir anchorSAM_cellpose \
+--wandb_run_name anchorSAM_cellpose \
 --coco_path /data/AllCellData/dataset/coco/cellpose \
 --train_json train_RLE.json \
 --val_json val_RLE.json \
